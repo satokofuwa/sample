0bee296623206a1088f3e37fb00492b5a3489987
satokofuwa <96944825+satokofuwa@users.noreply.github.com>
2022-02-25 09:12:23 +0900

    complete

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -11,7 +11,7 @@ class BlogsController < ApplicationController
   def create
     @blog=Blog.new(blog_params)
     if params[:back]
-      render :new
+      render :edit
     else
       if @blog.save
         redirect_to new_blog_path,notice:"ブログを作成しました！"
@@ -29,32 +29,35 @@ class BlogsController < ApplicationController
   end
 
   def edit
-    
+    @blog = Blog.find(params[:id])
   end
 
   def update
-   
-    if @blog.update(blog_params)
-      redirect_to blogs_path, notice:"ブログを編集しました"
-    else
+     if 
+      params[:back]
       render :edit
+      else
+      @blog.update(blog_params)
+      redirect_to blogs_path, notice:"ブログを編集しました!"
     end
   end
 
   def destroy
+   
     @blog.destroy
     redirect_to blogs_path, notice:"ブログを削除しました！"
   end
 
   def confirm
     @blog=Blog.new(blog_params)
+    @blog.id =params[:id]
     render :new if @blog.invalid?
   end
 
    private
 
    def blog_params
-    params.require(:blog).permit(:title, :content)
+    params.require(:blog).permit(:id,:title, :content)
    end
  
     def set_blog
